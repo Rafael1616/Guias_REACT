98ec8dbd40f5fe2d313fd50e4cecf7a580ebf562
Rafael1616 <sd0058032023@unab.edu.sv>
2025-03-05 20:24:22 -0600

    Guia 8: Listado de alumnos de un profesor

---
+++ NotasProyecto/WebApi/Controllers/AlumnoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using reactBackend.Models;
+using reactBackend.Repository;
+
+namespace WebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AlumnoController : ControllerBase
+    {
+
+        private AlumnoDao _dao = new AlumnoDao();
+
+        #region endPointAlumnoProfesor
+        [HttpGet("alumnoProfesor")]
+
+        public List<AlumnoProfesor> GetAlumnoProfesors(string usuario)
+        {
+            return _dao.alumnoProfesors(usuario);
+        }
+        #endregion
+
+    }
+}
--- NotasProyecto/reactBackend/Repository/AlumnoDao.cs
+++ NotasProyecto/reactBackend/Repository/AlumnoDao.cs
@@ -125,5 +125,28 @@ namespace reactBackend.Repository
             return consulta.ToList();
         }
         #endregion
+
+        #region leftJoinAlumnoMatriculaMateria
+        public List<AlumnoProfesor> alumnoProfesors(string nombreProfesor)
+        {
+            var listadoALumno = from a in contexto.Alumnos
+                                join m in contexto.Matriculas on a.Id equals m.AlumnoId
+                                join asig in contexto.Asignaturas on m.AsignaturaId equals asig.Id
+                                where asig.Profesor == nombreProfesor
+                                select new AlumnoProfesor
+                                {
+                                    Id = a.Id,
+                                    Dni = a.Dni,
+                                    Nombre = a.Nombre,
+                                    Direccion = a.Direccion,
+                                    Edad = a.Edad,
+                                    Email = a.Email,
+                                    asignatura = asig.Nombre
+                                };
+
+            return listadoALumno.ToList();
+        }
+        #endregion
+
     }
 }
